579e95383d8f984cd4052537df170cca97b55d35
Sangalang <sangalang@Sangalangs-MacBook-Air.local>
2024-04-20 12:24:19 -0400

    init repo objects

---
+++ script.js
@@ -0,0 +1,58 @@
+/****************** Navbar logic ***************/
+function navSlide() {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    //Toggle Nav
+    nav.classList.toggle("nav-active");
+
+    //Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+      }
+    });
+    //Burger Animation
+    burger.classList.toggle("toggle");
+  });
+}
+
+navSlide();
+
+//****************index.html
+
+//resume button
+// document.getElementById("resumeButton").addEventListener("click", () => {
+//   window.open("Jordan_Sangalang.pdf", "fullscreen=yes");
+// });
+
+// main cotact button
+document.getElementById("contactButton").addEventListener("click", function () {
+  window.location.href = "contact.html";
+});
+
+/***********contact page******************/
+const clearFormFields = () => {
+  const nameField = document.getElementById("name");
+  const mailField = document.getElementById("mail");
+  const msgField = document.getElementById("msg");
+  const sendButton = document.getElementById("msg-btn");
+
+  nameField.value = "";
+  mailField.value = "";
+  msgField.value = "";
+};
+
+// location.reload(clearFormFields());
+
+document.getElementById("msg-btn1").addEventListener("click", function () {
+  setTimeout(function () {
+    clearFormFields();
+  }, 1000);
+});
